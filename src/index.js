6e6021600d948a7f01e3450bf2f8535126fd66a8
Tushar Mathur <tusharmath@gmail.com>
2015-12-19 16:39:10 +0530

    export decorate util method

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@
 
 import {asStream} from './asStream'
 import {createDeclarative} from './createDeclarative'
+import {decorate} from './utility'
 
 export default {
-  asStream, createDeclarative
+  asStream, createDeclarative, decorate
 }
