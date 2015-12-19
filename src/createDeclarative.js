c3ebf08d9720308532f8cce8daa7bbd1e4821103
Tushar Mathur <tusharmath@gmail.com>
2015-12-19 20:38:37 +0530

    whitespace changes

--- src/createDeclarative.js
+++ src/createDeclarative.js
@@ -7,15 +7,14 @@
 import {filter, isFunction, initial, flow, curry, last} from 'lodash'
 import {asStream} from './asStream'
 
-export const createDeclarative = (func, ...declaratives) => {
-  return curry((...args) => {
-    declaratives = filter(declaratives, isFunction)
-    declaratives.push(asStream)
-    const params = initial(args)
-    const component = flow(...declaratives)(last(args))
-    component.prototype.getComponentStream = function (stream, dispose) {
-      func.call(this, stream, dispose, ...params)
-    }
-    return component
-  }, func.length - 1)
-}
+export const createDeclarative = (func, ...declaratives) => curry((...args) => {
+  declaratives = filter(declaratives, isFunction)
+  declaratives.push(asStream)
+  const params = initial(args)
+  const component = flow(...declaratives)(last(args))
+  component.prototype.getComponentStream = function (stream, dispose) {
+    func.call(this, stream, dispose, ...params)
+  }
+  return component
+}, func.length - 1)
+
