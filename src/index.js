512ba9669d5efcb805cb09e381998dbeddbe4e35
Tushar Mathur <tusharmath@gmail.com>
2015-12-19 18:53:14 +0530

    remove create declaratives

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@
 'use strict'
 
 import {asStream} from './asStream'
-import {createDeclarative} from './createDeclarative'
 import {decorate} from './utility'
 
 export default {
-  asStream, createDeclarative, decorate
+  asStream, decorate
 }
