464777c2ccbbbe09b2eea6e437bc94dee7490791
Tushar Mathur <tusharmath@gmail.com>
2015-12-20 19:13:52 +0530

    rename context to component

--- src/asStream.js
+++ src/asStream.js
@@ -21,30 +21,30 @@ export const addEventListener = (component, event, listener) => {
 export const asStream = (component) => {
   const prototype = component.prototype
   const listen = partial(addEventListener, component)
-  const stream = new BehaviorSubject({context: null, event: null, args: null})
+  const stream = new BehaviorSubject({component: null, event: null, args: null})
   const disposables = new WeakMap()
   const addDisposable = function (...args) {
     filter(args, isDisposable).forEach(x => disposables.get(this).push(x))
   }
   listen('componentWillMount', function (...args) {
     disposables.set(this, [])
-    stream.onNext({context: this, event: 'WILL_MOUNT', args})
-    this.getComponentStream(stream.filter(x => x.context === this), addDisposable.bind(this))
+    stream.onNext({component: this, event: 'WILL_MOUNT', args})
+    this.getComponentStream(stream.filter(x => x.component === this), addDisposable.bind(this))
   })
   listen('componentDidMount', function (...args) {
-    stream.onNext({context: this, event: 'DID_MOUNT', args})
+    stream.onNext({component: this, event: 'DID_MOUNT', args})
   })
   listen('componentWillReceiveProps', function (...args) {
-    stream.onNext({context: this, event: 'WILL_RECEIVE_PROPS', args})
+    stream.onNext({component: this, event: 'WILL_RECEIVE_PROPS', args})
   })
   listen('componentWillUpdate', function (...args) {
-    stream.onNext({context: this, event: 'WILL_UPDATE', args})
+    stream.onNext({component: this, event: 'WILL_UPDATE', args})
   })
   listen('componentDidUpdate', function (...args) {
-    stream.onNext({context: this, event: 'DID_UPDATE', args})
+    stream.onNext({component: this, event: 'DID_UPDATE', args})
   })
   listen('componentWillUnmount', function (...args) {
-    stream.onNext({context: this, event: 'WILL_UNMOUNT', args})
+    stream.onNext({component: this, event: 'WILL_UNMOUNT', args})
     each(disposables.get(this), x => x.dispose())
     disposables.delete(this)
   })
