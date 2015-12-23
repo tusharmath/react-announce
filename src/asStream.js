1511356d78a7c8d36e8b5dcd9d35dc93e5628500
Tushar Mathur <tusharmath@gmail.com>
2015-12-23 14:25:09 +0530

    es5 for asStream

--- src/asStream.js
+++ src/asStream.js
@@ -4,7 +4,7 @@
 
 'use strict'
 const _ = require('lodash')
-const BehaviorSubject = require ('rx').BehaviorSubject
+const BehaviorSubject = require('rx').BehaviorSubject
 const addEventListener = require('./addEventListener')
 
 const isDisposable = i => _.isFunction(_.get(i, 'dispose'))
@@ -21,27 +21,34 @@ module.exports = component => {
   const listen = _.partial(addEventListener, component)
   const stream = component[STREAM_SYMBOL] = new BehaviorSubject({component: null, event: null, args: null})
   const disposables = new WeakMap()
-  const addDisposable = function (...args) {
+  const addDisposable = function () {
+    const args = _.toArray(arguments)
     _.filter(args, isDisposable).forEach(x => disposables.get(this).push(x))
   }
-  listen('componentWillMount', function (...args) {
+  listen('componentWillMount', function () {
+    const args = _.toArray(arguments)
     disposables.set(this, [])
     stream.onNext({component: this, event: 'WILL_MOUNT', args})
     this.getComponentStream(stream.filter(x => x.component === this), addDisposable.bind(this))
   })
-  listen('componentDidMount', function (...args) {
+  listen('componentDidMount', function () {
+    const args = _.toArray(arguments)
     stream.onNext({component: this, event: 'DID_MOUNT', args})
   })
-  listen('componentWillReceiveProps', function (...args) {
+  listen('componentWillReceiveProps', function () {
+    const args = _.toArray(arguments)
     stream.onNext({component: this, event: 'WILL_RECEIVE_PROPS', args})
   })
-  listen('componentWillUpdate', function (...args) {
+  listen('componentWillUpdate', function () {
+    const args = _.toArray(arguments)
     stream.onNext({component: this, event: 'WILL_UPDATE', args})
   })
-  listen('componentDidUpdate', function (...args) {
+  listen('componentDidUpdate', function () {
+    const args = _.toArray(arguments)
     stream.onNext({component: this, event: 'DID_UPDATE', args})
   })
-  listen('componentWillUnmount', function (...args) {
+  listen('componentWillUnmount', function () {
+    const args = _.toArray(arguments)
     stream.onNext({component: this, event: 'WILL_UNMOUNT', args})
     _.each(disposables.get(this), x => x.dispose())
     disposables.delete(this)
