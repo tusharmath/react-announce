168c50514bc8aa16c60b45ea6b2aadecc8172d78
Tushar Mathur <tusharmath@gmail.com>
2016-02-21 12:54:41 +0530

    perf(createDeclarative): ignore rest of the params
    not a popular feature
    
    BREAKING CHANGE: createDeclarative does not support chaing of decorators anymore

--- src/createDeclarative.js
+++ src/createDeclarative.js
@@ -8,14 +8,10 @@ const createComponentStream = require('./createComponentStream')
 const addEventListener = require('./addEventListener')
 
 module.exports = function (func) {
-  const declaratives = [].slice
-    .call(arguments, 1)
-    .filter(x => typeof x === 'function')
-  declaratives.push(createComponentStream)
   return function () {
     const params = [].slice.call(arguments)
     return function (component) {
-      component = declaratives.reduce((c, d) => d(c), component)
+      component = createComponentStream(component)
       addEventListener(component, 'getInstanceStream', function (stream, dispose) {
         func.apply(this, [stream, dispose].concat(params))
       })
